b9663200a37e2270067d7f9b6f199a0be9b4e5d9
ko.horii <sa120073@bbt.ohmae.ac.jp>
2019-02-15 02:39:10 +0000

    user registation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,12 @@
       else
         flash.now[:danger] = 'ユーザの登録に失敗しました。'
         render :new
+      end
     end
-  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+end
