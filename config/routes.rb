6a6ff189b2150fbcfd3cbffab4fc35865e859be9
ko.horii <sa120073@bbt.ohmae.ac.jp>
2019-02-18 11:50:55 +0000

    ranking

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root to: 'toppages#index'
- 
+    
+    get 'rankings/want', to: 'rankings#want'
+    get 'rankings/have', to: 'rankings#have'
+     
     get 'login', to: 'sessions#new'
     post 'login', to: 'sessions#create'
     delete 'logout', to: 'sessions#destroy'
